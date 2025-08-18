6366c03fb5f096bddd8b5ced955d057fc240c4c8
Wenyan_Keith <138771870+Mosxter@users.noreply.github.com>
2025-08-18 20:55:30 +0800

    Add files via upload

---
+++ childpage/TapRhythm/src/main.js
@@ -0,0 +1,31 @@
+import { Start } from './scenes/Start.js';
+
+const config = {
+    type: Phaser.AUTO,
+    title: 'Overlord Rising',
+    description: '',
+    parent: 'game-container',
+    width: 1920,
+    height: 1080,
+    backgroundColor: '#000000',
+    pixelArt: false,
+    scene: [
+        Start
+    ],
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        min: {
+            width: 1600,
+            height: 900
+        },
+        max: {
+            width: 1920,
+            height: 1080
+        },
+        zoom: 1
+    }
+}
+
+new Phaser.Game(config);
+            
